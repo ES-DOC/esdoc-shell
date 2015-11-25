0b0454d54496c39bca15dca9c5546c74ae56a964
Mark Morgan <momipsl@ipsl.jussieu.fr>
2015-11-25 13:51:43 +0100

    made cmip6 document publishing target directories implicit

--- bash/pyesdoc/publish_cmip6_documents.sh
+++ bash/pyesdoc/publish_cmip6_documents.sh
@@ -7,13 +7,14 @@ source $ESDOC_HOME/bash/init.sh
 main()
 {
 	log "PYESDOC : publishing cmip6 documents ..."
-	# activate_venv pyesdoc
-	log $1
-	rm -rf $1/*.json
-	rm -rf $1/*.xml
-	python $ESDOC_DIR_PYESDOC/jobs/run_publish_cmip6_documents.py --archive-dir=$1 --spreadsheet=$2
-	log "PYESDOC : cmip6 documents published"
+	activate_venv pyesdoc
+	declare io_dir=$ESDOC_DIR_REPOS/esdoc-docs-cmip6/experiments/extracted
+	declare spreadsheet=$ESDOC_DIR_REPOS/esdoc-docs-cmip6/experiments/cmip6-experiments-v2.xlsx
+	rm -rf $io_dir/*.json
+	rm -rf $io_dir/*.xml
+	python $ESDOC_DIR_PYESDOC/jobs/run_publish_cmip6_documents.py --archive-dir=$io_dir --spreadsheet=$spreadsheet
+	log "PYESDOC : cmip6 documents published to "$io_dir
 }
 
 # Invoke entry point.
-main $1 $2
+main
