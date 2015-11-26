130a9c2148ea431fcb0417278dc00e9fac163358
Mark Morgan <momipsl@ipsl.jussieu.fr>
2015-11-26 17:29:57 +0100

    removed config update from stack update command

--- bash/stack/update.sh
+++ bash/stack/update.sh
@@ -9,7 +9,6 @@ main()
 	log "UPDATING STACK"
 
 	source $ESDOC_HOME/bash/stack/update_shell.sh
-	source $ESDOC_HOME/bash/stack/update_config.sh
 	source $ESDOC_HOME/bash/stack/update_repos.sh
 	source $ESDOC_HOME/bash/stack/upgrade_venvs.sh
 
