86a618302dab13cd36f08ef6c38343e5effd29c0
Mark Morgan <momipsl@ipsl.jussieu.fr>
2016-02-17 12:14:25 +0100

    Moved CMIP6 mind map generator to esdoc-mp

--- bash/mp/write_cmip6_mindmap.sh
+++ bash/mp/write_cmip6_mindmap.sh
@@ -6,9 +6,21 @@ source $ESDOC_HOME/bash/init.sh
 # Main entry point.
 main()
 {
-	python $ESDOC_HOME/bash/mp/write_cmip6_mindmap.py --dest=$ESDOC_DIR_REPOS/esdoc-cim/vocabs/cmip6/mindmaps --domain=$1
+	# Set stylesheet & I/O destination.
+	if [ "$2" ]; then
+		declare stylesheet=$2
+	else
+		declare stylesheet=$ESDOC_DIR_BASH/mp/write_cmip6_mindmap.conf
+	fi
+	declare dest=$ESDOC_DIR_REPOS/esdoc-cim/vocabs/cmip6/mindmaps
+
+	# Invoke mindmap generator.
+	activate_venv mp
+	python $ESDOC_DIR_MP"/esdoc_mp/vocabs/cmip6/mindmap.py" --dest=$dest  --domain=$1 --stylesheet=$stylesheet
+
+	# Copy mindmaps to esdoc-docs repo.
 	cp $ESDOC_DIR_REPOS/esdoc-cim/vocabs/cmip6/mindmaps/* $ESDOC_DIR_REPOS/esdoc-docs/cmip6/mindmaps
 }
 
 # Invoke entry point.
-main $1
+main $1 $2
