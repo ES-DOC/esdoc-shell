3da9afa9e01cd4b03818a00f6890f4b88ccd2b39
Mark Morgan <momipsl@ipsl.jussieu.fr>
2016-02-18 12:06:06 +0100

    factored out cmip6 related jobs to dedicated bash sub-folder

--- bash/cmip6/archive_documents.sh
+++ bash/cmip6/archive_documents.sh
@@ -7,12 +7,16 @@ source $ESDOC_HOME/bash/init.sh
 main()
 {
 	log "PYESDOC : writing cmip6 documents to archive ..."
-	declare source_dir=$ESDOC_DIR_REPOS/esdoc-docs-cmip6/experiments/extracted
+
+	declare source_dir=$ESDOC_DIR_REPOS/esdoc-docs/cmip6/experiments/extracted
 	declare target_dir=$ESDOC_DIR_REPOS/esdoc-archive/esdoc/cmip6-draft/spreadsheet
+
 	mkdir -p $target_dir
 	rm -rf $target_dir/*.json
+
 	activate_venv pyesdoc
-	python $ESDOC_DIR_PYESDOC/jobs/run_archive_cmip6_documents.py --source-dir=$source_dir --target-dir=$target_dir
+	python $ESDOC_DIR_PYESDOC/jobs/run_cmip6_archive_documents.py --source-dir=$source_dir --target-dir=$target_dir
+
 	log "PYESDOC : cmip6 documents written to archive: "$target_dir
 }
 
---
+++ bash/cmip6/write_experiments.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Import utils.
+source $ESDOC_HOME/bash/init.sh
+
+# Main entry point.
+main()
+{
+	log "PYESDOC : writing cmip6 experiments ..."
+
+	declare io_dir=$ESDOC_DIR_REPOS/esdoc-docs/cmip6/experiments/extracted
+	declare spreadsheet=$ESDOC_DIR_REPOS/esdoc-docs/cmip6/experiments/cmip6-experiments-v8.xlsx
+
+	rm -rf $io_dir/*.json
+	rm -rf $io_dir/*.xml
+
+	activate_venv pyesdoc
+	python $ESDOC_DIR_PYESDOC/jobs/run_cmip6_write_experiments.py --archive-dir=$io_dir --spreadsheet=$spreadsheet
+
+	log "PYESDOC : cmip6 experiments written to "$io_dir
+}
+
+# Invoke entry point.
+main
