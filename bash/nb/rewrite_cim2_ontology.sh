716a12599f1f90908d483f979f3ae8a7653b9f96
Mark Morgan <momipsl@ipsl.jussieu.fr>
2016-03-01 08:30:33 +0100

    updated destination of notebook cim v2 schema location

--- bash/nb/rewrite_cim2_ontology.sh
+++ bash/nb/rewrite_cim2_ontology.sh
@@ -8,7 +8,9 @@ main()
 {
 	export PYTHONPATH=$PYTHONPATH:$ESDOC_DIR_NB
 
-	python $ESDOC_HOME/bash/nb/rewrite_cim2_ontology.py --dest=$ESDOC_DIR_REPOS/esdoc-mp/esdoc_mp/schemas/cim/v2
+	declare dest=$ESDOC_DIR_REPOS/esdoc-mp/esdoc_mp/ontologies/schemas/cim/v2
+
+	python $ESDOC_HOME/bash/nb/rewrite_cim2_ontology.py --dest=$dest
 }
 
 # Invoke entry point.
