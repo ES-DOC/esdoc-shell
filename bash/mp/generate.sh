7a8ea5fbf4e65531734b36b1d304f9fb110ee086
Mark Morgan <momipsl@ipsl.jussieu.fr>
2016-03-01 08:16:21 +0100

    1.  Reinstated esdoc-mp-generate command.  2.  Improved esdoc-mp-generate logging.  3.  Copied generated qxml to dedicated folder.

--- bash/mp/generate.sh
+++ bash/mp/generate.sh
@@ -9,11 +9,29 @@ main()
 	declare ontology=$1
 	declare version=$2
 	declare language=$3
+
+	if [ $language = "python" ]; then
+		declare dest=$ESDOC_DIR_PYESDOC/pyesdoc/ontologies/$ontology
+	elif [ $language = "qxml" ]; then
+		declare dest=$ESDOC_DIR_RESOURCES/qtn-$ontology-v$version.xml
+	fi
+	if [ $language = "python" ]; then
+		declare data=$ESDOC_DIR_TMP/$ontology/v$version
+	elif [ $language = "qxml" ]; then
+		declare data=$ESDOC_DIR_TMP/$ontology/v$version/$ontology"_"$version.xml
+	fi
+
 	activate_venv mp
 	python "$ESDOC_DIR_MP/esdoc_mp" -s $ontology -v $version -l $language -o $ESDOC_DIR_TMP
-	cp -r "$ESDOC_DIR_TMP/$ontology/v$version" "$ESDOC_DIR_PYESDOC/pyesdoc/ontologies/$ontology"
+	cp -r $data $dest
+
+	log_banner
+	log "generated artefacts copied to @ "$dest
+	log_banner
+
 	find $ESDOC_DIR_PYESDOC -type f -name "*.pyc" -exec rm -f {} \;
 	find $ESDOC_DIR_PYESDOC -type f -name "*.pye" -exec rm -f {} \;
+	reset_tmp
 }
 
 # Invoke entry point.
