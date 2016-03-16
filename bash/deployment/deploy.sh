4e384729429d2b6cae5388d835ee43dcc6ef6cf3
Mark Morgan <momipsl@ipsl.jussieu.fr>
2016-03-16 15:38:08 +0100

    Updated cim v1 psd location

--- bash/deployment/deploy.sh
+++ bash/deployment/deploy.sh
@@ -138,7 +138,7 @@ _install_source_static()
 	cp -r $ESDOC_DIR_REPOS/esdoc-web/static/* $ESDOC_DIR_WEBAPPS/$1_$2_static
 	cp -r $ESDOC_DIR_REPOS/esdoc-web/plugin/bin/latest/* $ESDOC_DIR_WEBAPPS/$1_$2_static
 	mkdir -p $ESDOC_DIR_WEBAPPS/$1_$2_static/ontologies/cim/1
-	cp -r $ESDOC_DIR_REPOS/esdoc-cim/ontologies/v1/*.xsd $ESDOC_DIR_WEBAPPS/$1_$2_static/ontologies/cim/1
+	cp -r $ESDOC_DIR_REPOS/esdoc-cim/ontologies/v1/xsd/*.xsd $ESDOC_DIR_WEBAPPS/$1_$2_static/ontologies/cim/1
 
 	# ... viewer micro-site
 	cp -r $ESDOC_DIR_REPOS/esdoc-web/viewer/* $ESDOC_DIR_WEBAPPS/$1_$2_view
