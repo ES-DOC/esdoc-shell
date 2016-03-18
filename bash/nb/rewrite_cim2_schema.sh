4545d5b9743028efafebb21f946537cc3f6024fb
Mark Morgan <momipsl@ipsl.jussieu.fr>
2016-03-18 11:13:56 +0100

    renamed cmip6 vocab rewriter command

--- bash/nb/rewrite_cim2_schema.sh
+++ bash/nb/rewrite_cim2_schema.sh
@@ -9,7 +9,7 @@ main()
 	export PYTHONPATH=$PYTHONPATH:$ESDOC_DIR_NB
 
 	declare dest=$ESDOC_DIR_REPOS/esdoc-cim/v2/schema
-	python $ESDOC_HOME/bash/nb/rewrite_cim2_ontology.py --dest=$dest
+	python $ESDOC_HOME/bash/nb/rewrite_cim2_schema.py --dest=$dest
 }
 
 # Invoke entry point.
