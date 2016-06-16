76a0ea25aa8b0bee1d1c4f315142b2cd9f5b1ffb
Mark Morgan <momipsl@ipsl.jussieu.fr>
2016-06-16 17:26:10 +0200

    Logging related update

--- bash/cmip6/archive_documents.sh
+++ bash/cmip6/archive_documents.sh
@@ -6,7 +6,7 @@ source $ESDOC_HOME/bash/init.sh
 # Main entry point.
 main()
 {
-	log "PYESDOC : writing cmip6 documents to archive ..."
+	log "PYESDOC : CMIP6 : archiving documents ..."
 
 	declare source_dir=$ESDOC_DIR_REPOS/esdoc-docs/cmip6/experiments/extracted
 	declare target_dir=$ESDOC_DIR_REPOS/esdoc-archive/esdoc/cmip6-draft/spreadsheet
@@ -17,7 +17,7 @@ main()
 	activate_venv pyesdoc
 	python $ESDOC_DIR_PYESDOC/jobs/run_cmip6_archive_documents.py --source-dir=$source_dir --target-dir=$target_dir
 
-	log "PYESDOC : cmip6 documents written to archive: "$target_dir
+	log "PYESDOC : CMIP6 : archived documents @ "$target_dir
 }
 
 # Invoke entry point.
