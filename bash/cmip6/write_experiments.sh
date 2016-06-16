fc05cf8750c51a3dcc0a79df72d040b2c25e6d18
Mark Morgan <momipsl@ipsl.jussieu.fr>
2016-06-16 14:55:00 +0200

    Removed version identifier argument when generating experiment documents for cmip6

--- bash/cmip6/write_experiments.sh
+++ bash/cmip6/write_experiments.sh
@@ -9,16 +9,17 @@ main()
 	log "PYESDOC : writing cmip6 experiments ..."
 
 	declare io_dir=$ESDOC_DIR_REPOS/esdoc-docs/cmip6/experiments/extracted
-	declare spreadsheet=$ESDOC_DIR_REPOS/esdoc-docs/cmip6/experiments/cmip6-experiments-v$1.xlsx
+	declare spreadsheet=$ESDOC_DIR_REPOS/esdoc-docs/cmip6/experiments/cmip6-experiments-v32.xlsx
+	declare identifiers=$ESDOC_DIR_REPOS/esdoc-docs/cmip6/experiments/cmip6-experiments-document-identifiers.txt
 
 	rm -rf $io_dir/*.json
 	rm -rf $io_dir/*.xml
 
 	activate_venv pyesdoc
-	python $ESDOC_DIR_PYESDOC/jobs/run_cmip6_write_experiments.py --io-dir=$io_dir --spreadsheet=$spreadsheet
+	python $ESDOC_DIR_PYESDOC/jobs/run_cmip6_write_experiments.py --io-dir=$io_dir --spreadsheet=$spreadsheet --identifiers=$identifiers
 
 	log "PYESDOC : cmip6 experiments written to "$io_dir
 }
 
 # Invoke entry point.
-main $1
+main
