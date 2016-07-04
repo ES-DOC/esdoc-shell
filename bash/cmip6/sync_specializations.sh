6dbea8c87f93f98a89f1612d7886b14f1a791580
Mark Morgan <momipsl@ipsl.jussieu.fr>
2016-07-04 17:53:54 +0200

    updated cmip6 specialisations synched

--- bash/cmip6/sync_specializations.sh
+++ bash/cmip6/sync_specializations.sh
@@ -13,27 +13,34 @@ main()
 		'ocean'
 	)
 
-	# Remove existing.
+	# Sync definitions.
 	for specialization in "${CMIP6_SPECIALIZATIONS[@]}"
 	do
+		# ... remove previous
 		rm $ESDOC_DIR_REPOS/esdoc-cim/v2/specializations/cmip6/definitions/$specialization*.py
-		rm $ESDOC_DIR_REPOS/esdoc-cim/v2/specializations/cmip6/mindmaps/$specialization.mm
-		rm $ESDOC_DIR_REPOS/esdoc-cim/v2/specializations/cmip6/config/$specialization.json
 		rm $ESDOC_DIR_REPOS/esdoc-mp/esdoc_mp/specializations/cmip6/$specialization*.py
-	done
-
-	# Generate new.
-	for specialization in "${CMIP6_SPECIALIZATIONS[@]}"
-	do
-		python $ESDOC_DIR_REPOS/cmip6-specializations-$specialization/generate --type=mm
-		python $ESDOC_DIR_REPOS/cmip6-specializations-$specialization/generate --type=json
-	done
-
-	# Copy most recent.
-	for specialization in "${CMIP6_SPECIALIZATIONS[@]}"
-	do
+		# ... copy current
 		cp $ESDOC_DIR_REPOS/cmip6-specializations-$specialization/$specialization*.py $ESDOC_DIR_REPOS/esdoc-cim/v2/specializations/cmip6/definitions
 		cp $ESDOC_DIR_REPOS/cmip6-specializations-$specialization/$specialization*.py $ESDOC_DIR_REPOS/esdoc-mp/esdoc_mp/specializations/cmip6
+	done
+
+	# Sync generated.
+	for specialization in "${CMIP6_SPECIALIZATIONS[@]}"
+	do
+		# ... remove previous
+		rm $ESDOC_DIR_REPOS/esdoc-cim/v2/specializations/cmip6/mindmaps/$specialization.mm
+		rm $ESDOC_DIR_REPOS/esdoc-cim/v2/specializations/cmip6/config/$specialization.json
+		rm $ESDOC_DIR_REPOS/esdoc-cim/v2/specializations/cmip6/csv/$specialization*.csv
+		# ... regenerate
+		python $ESDOC_DIR_REPOS/cmip6-specializations-$specialization/generate --type=mm
+		python $ESDOC_DIR_REPOS/cmip6-specializations-$specialization/generate --type=json
+		python $ESDOC_DIR_REPOS/cmip6-specializations-$specialization/generate --type=ids-level-1
+		python $ESDOC_DIR_REPOS/cmip6-specializations-$specialization/generate --type=ids-level-2
+		python $ESDOC_DIR_REPOS/cmip6-specializations-$specialization/generate --type=ids-level-3
+		# ... copy generated
+		cp $ESDOC_DIR_REPOS/cmip6-specializations-$specialization/_$specialization-ids-level-1.csv $ESDOC_DIR_REPOS/esdoc-cim/v2/specializations/cmip6/csv/$specialization-ids-level-1.csv
+		cp $ESDOC_DIR_REPOS/cmip6-specializations-$specialization/_$specialization-ids-level-2.csv $ESDOC_DIR_REPOS/esdoc-cim/v2/specializations/cmip6/csv/$specialization-ids-level-2.csv
+		cp $ESDOC_DIR_REPOS/cmip6-specializations-$specialization/_$specialization-ids-level-3.csv $ESDOC_DIR_REPOS/esdoc-cim/v2/specializations/cmip6/csv/$specialization-ids-level-3.csv
 		cp $ESDOC_DIR_REPOS/cmip6-specializations-$specialization/_$specialization.mm $ESDOC_DIR_REPOS/esdoc-cim/v2/specializations/cmip6/mindmaps/$specialization.mm
 		cp $ESDOC_DIR_REPOS/cmip6-specializations-$specialization/_$specialization.json $ESDOC_DIR_REPOS/esdoc-cim/v2/specializations/cmip6/config/$specialization.json
 	done
