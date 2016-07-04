d224b3f941b135f85133be7172fc4fce2e8453c5
Mark Morgan <momipsl@ipsl.jussieu.fr>
2016-07-04 17:43:32 +0200

    updated cmip6 specialisations synched

--- bash/cmip6/sync_specializations.sh
+++ bash/cmip6/sync_specializations.sh
@@ -8,13 +8,35 @@ main()
 {
 	log "PYESDOC : syncing cmip6 specializations ..."
 
-	# Copy from satellite repos to esdoc-cim repo.
-	cp $ESDOC_DIR_REPOS/cmip6-specializations-atmosphere/atmosphere*.py $ESDOC_DIR_REPOS/esdoc-cim/v2/specializations/cmip6/definitions
-	cp $ESDOC_DIR_REPOS/cmip6-specializations-ocean/ocean*.* $ESDOC_DIR_REPOS/esdoc-cim/v2/specializations/cmip6/definitions
-	cp $ESDOC_DIR_REPOS/cmip6-specializations-seaice/seaice*.* $ESDOC_DIR_REPOS/esdoc-cim/v2/specializations/cmip6/definitions
+	# Set of specializations.
+	declare -a CMIP6_SPECIALIZATIONS=(
+		'ocean'
+	)
 
-	# Copy all to esdoc-mp repo.
-	cp $ESDOC_DIR_REPOS/esdoc-cim/v2/specializations/cmip6/definitions/*.py $ESDOC_DIR_REPOS/esdoc-mp/esdoc_mp/specializations/cmip6/schema
+	# Remove existing.
+	for specialization in "${CMIP6_SPECIALIZATIONS[@]}"
+	do
+		rm $ESDOC_DIR_REPOS/esdoc-cim/v2/specializations/cmip6/definitions/$specialization*.py
+		rm $ESDOC_DIR_REPOS/esdoc-cim/v2/specializations/cmip6/mindmaps/$specialization.mm
+		rm $ESDOC_DIR_REPOS/esdoc-cim/v2/specializations/cmip6/config/$specialization.json
+		rm $ESDOC_DIR_REPOS/esdoc-mp/esdoc_mp/specializations/cmip6/$specialization*.py
+	done
+
+	# Generate new.
+	for specialization in "${CMIP6_SPECIALIZATIONS[@]}"
+	do
+		python $ESDOC_DIR_REPOS/cmip6-specializations-$specialization/generate --type=mm
+		python $ESDOC_DIR_REPOS/cmip6-specializations-$specialization/generate --type=json
+	done
+
+	# Copy most recent.
+	for specialization in "${CMIP6_SPECIALIZATIONS[@]}"
+	do
+		cp $ESDOC_DIR_REPOS/cmip6-specializations-$specialization/$specialization*.py $ESDOC_DIR_REPOS/esdoc-cim/v2/specializations/cmip6/definitions
+		cp $ESDOC_DIR_REPOS/cmip6-specializations-$specialization/$specialization*.py $ESDOC_DIR_REPOS/esdoc-mp/esdoc_mp/specializations/cmip6
+		cp $ESDOC_DIR_REPOS/cmip6-specializations-$specialization/_$specialization.mm $ESDOC_DIR_REPOS/esdoc-cim/v2/specializations/cmip6/mindmaps/$specialization.mm
+		cp $ESDOC_DIR_REPOS/cmip6-specializations-$specialization/_$specialization.json $ESDOC_DIR_REPOS/esdoc-cim/v2/specializations/cmip6/config/$specialization.json
+	done
 
 	log "PYESDOC : syncing cmip6 specializations"
 }
