ae2bb4bc53287fe5d3dbdb5f698a6e5365029ad8
Mark Morgan <momipsl@ipsl.jussieu.fr>
2016-10-31 13:02:05 +0100

    Modified destinations of esdoc-mp generated assets

--- bash/mp/generate.sh
+++ bash/mp/generate.sh
@@ -23,17 +23,23 @@ main()
 	if [ $language = "python" ]; then
 		declare dest=$ESDOC_DIR_PYESDOC/pyesdoc/ontologies/$ontology
 		cp -r $data $dest
-		log "generated artefacts copied to @ "$dest
+		rm $dest/v$version/extended_schema*
+		log "pyesdoc artefacts copied to @ "$dest/v$version
+
+		declare dest=$ESDOC_DIR_CIM/v$version/extended-schema
+		cp -r $data/extended_schema* $dest
+		log "extended schema copied to @ "$dest
+
 	elif [ $language = "qxml" ]; then
 		declare dest=$ESDOC_DIR_CIM/v$version/questionnaire/$ontology-v$version-q-config.xml
 		cp -r $data $dest
-		log "generated artefacts copied to @ "$dest
+		log "qxml artefacts copied to @ "$dest
 	fi
 	log_banner
 
 	find $ESDOC_DIR_PYESDOC -type f -name "*.pyc" -exec rm -f {} \;
 	find $ESDOC_DIR_PYESDOC -type f -name "*.pye" -exec rm -f {} \;
-	reset_tmp
+	# reset_tmp
 }
 
 # Invoke entry point.
