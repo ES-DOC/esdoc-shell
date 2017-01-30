04e8a962bde7976ccc2f10358e035db3a45e9578
Chris Blanton <chris.blanton@noaa.gov>
2017-01-29 21:41:16 -0500

    Use official CMIP CV as source for institute/model/realm information to generate ipython notebooks, first draft attempt

--- bash/cmip6/write_model_notebooks.py
+++ bash/cmip6/write_model_notebooks.py
@@ -17,7 +17,9 @@ import os
 from tornado import template
 
 import pyesdoc
-from pyesdoc.ipython.model_realm_properties import NotebookData
+from pyesdoc.ipython.model_realm import NotebookData
+from pyesdoc.cv.archive import load_collection as load_cv_collection
+from pyesdoc.cv.model import Collection
 
 
 
@@ -51,15 +53,17 @@ def _main(args):
     if not os.path.isfile(args.cfg_fpath):
         raise ValueError("Configration file does not exist")
 
-    # Load configuration file.
-    with open(args.cfg_fpath, 'r') as fstream:
-        cfg = json.loads(fstream.read())
+    # Read CMIP6 JSON/CV
+    models = _get_cv()
+
+    # should be pulled in from someplace else, not set here
+    mip_era = 'cmip6'
 
     # For each institute / model / realm combination, emit a notebook.
-    for institute, model in [(i.split(":")[0], i.split(":")[1]) for i in cfg['models']]:
-        for realm in cfg['realms']:
+    for institute, model, realms in [(i['institute'], i['name'], i['realms']) for i in models]:
+        for realm in realms:
             # ... load notebook data;
-            data = _get_data(args.output_dir, cfg['mip_era'], institute, model, realm)
+            data = _get_data(args.output_dir, mip_era, institute, model, realm)
 
             # ... write notebook content;
             _write(args.output_dir, data)
@@ -106,6 +110,70 @@ def _get_content(data):
     # Return prettified content.
     return json.dumps(json.loads(content), indent=4)
 
+
+def _get_data(output_dir, mip_era, institute, source_id, realm):
+    """Returns notebook data wrapper.
+
+    """
+    # Instantiate wrapper.
+    data = NotebookData(mip_era, institute, source_id, realm)
+
+    # Initialise from previously saved output.
+    fname = "{}--{}--{}.json".format(institute, source_id, realm)
+    fpath = os.path.join(output_dir, "output")
+    fpath = os.path.join(fpath, fname)
+    if os.path.isfile(fpath):
+        with open(fpath, 'r') as fstream:
+            data.from_dict(json.loads(fstream.read()))
+
+    return data
+
+
+def _write(output_dir, mip_era, institute, source_id, realm, content):
+    """Writes notebook content to file system.
+
+    """
+    fname = "{}--{}--{}.ipynb".format(institute, source_id, realm)
+    fpath = os.path.join(output_dir, "notebooks")
+    fpath = os.path.join(fpath, fname)
+    with open(fpath, 'w') as fstream:
+        fstream.write(content)
+
+def print_attributes(obj):
+    print "Type: ", type(obj)
+    for attr in obj.__dict__:
+        print attr, getattr(obj, attr)
+
+def _get_cv():
+    models = []
+
+    # possible realms- should be set to definitive list, not this
+    #realms = ['aerosol', 'atmosphere', 'atmospheric_chemistry', 'land_ice',
+    #          'land_surface', 'ocean', 'ocean_biogeochemistry', 'sea_ice']
+    # should work, but sea_ice / seaice
+    #realms = ['atmosphere', 'ocean', 'sea_ice']
+    realms = ['atmosphere', 'ocean']
+
+    # get the CMIP6 source_id JSON
+    cv_models = load_cv_collection('wcrp', 'cmip6', 'source-id')
+    assert isinstance(cv_models, Collection)
+
+    # for each participating model, record the name, institutes, and realms simulated
+    for cv_model in cv_models.terms:
+
+        # foreach institute that runs the model
+        for institute in cv_model.data['institution_id']:
+            model = dict()
+            model['name'] = cv_model.data['source_id'].lower()
+            model['institute'] = institute.lower()
+            model['realms'] = []
+            for realm in realms:
+                if (cv_model.data[realm] and cv_model.data[realm] != 'None'):
+                    model['realms'].append(realm)
+
+            models.append(model)
+
+    return models
 
 # Entry point.
 if __name__ == '__main__':
