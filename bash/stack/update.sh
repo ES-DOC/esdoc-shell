18147d64aa5bf09c374e1044309039459d38239e
Mark A. Greenslade <momipsl@ipsl.jussieu.fr>
2017-07-11 23:11:48 +0200

    Updated shell install

--- bash/stack/update.sh
+++ bash/stack/update.sh
@@ -19,14 +19,15 @@ _update_repo()
 	declare target=$1
 	declare dir_repo=$2"/$1"
 	declare gh_repo=$3"/$1.git"
+	declare repo_type=$4
 	if [ -d "$dir_repo" ]; then
-		log "Updating core repo: $target"
+		log "Updating $repo_type repo: $target"
 		set_working_dir $dir_repo
 		git pull -q
 		remove_files "*.pyc"
 		set_working_dir
 	else
-		log "Installing core repo: $target"
+		log "Installing $repo_type repo: $target"
 		rm -rf $dir_repo
 		git clone -q $gh_repo $dir_repo
 	fi
@@ -41,19 +42,19 @@ _update_repos()
 	mkdir -p $ESDOC_DIR_REPOS_CORE
 	for repo in "${ESDOC_REPOS[@]}"
 	do
-		_update_repo $repo $ESDOC_DIR_REPOS "https://github.com/ES-DOC"
+		_update_repo $repo $ESDOC_DIR_REPOS "https://github.com/ES-DOC" "core"
 	done
 
 	mkdir -p $ESDOC_DIR_REPOS_CMIP6
 	for repo in "${ESDOC_REPOS_CMIP6[@]}"
 	do
-		_update_repo $repo $ESDOC_DIR_REPOS_CMIP6 "https://github.com/ES-DOC"
+		_update_repo $repo $ESDOC_DIR_REPOS_CMIP6 "https://github.com/ES-DOC" "cmip6"
 	done
 
 	mkdir -p $ESDOC_DIR_REPOS/institutional
 	for institution in "${INSTITUTION_ID[@]}"
 	do
-		_update_repo $institution $ESDOC_DIR_REPOS_INSTITUTIONAL "https://github.com/ES-DOC-INSTITUTIONAL"
+		_update_repo $institution $ESDOC_DIR_REPOS_INSTITUTIONAL "https://github.com/ES-DOC-INSTITUTIONAL" "institutional"
 	done
 
 	log "UPDATED REPOS"
