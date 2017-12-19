d4bc01d86cda97554d8f0bb245326f7fea67dec9
Mark A. Greenslade <momipsl@ipsl.jussieu.fr>
2017-12-19 09:53:57 +0100

    Updated cmip6 specializations syncing

--- bash/cmip6/specializations/sync_definitions.sh
+++ bash/cmip6/specializations/sync_definitions.sh
@@ -11,34 +11,43 @@ main()
 {
 	log "CMIP6-SPECS : syncing definitions ..."
 
+	log "CMIP6-SPECS : syncing py files ..."
 	for specialization in "${CMIP6_SPECIALIZATIONS[@]}"
 	do
 		rm $ESDOC_DIR_REPOS/esdoc-py-client/pyesdoc/mp/specializations/cmip6/$specialization*.py
-		rm $ESDOC_DIR_REPOS/esdoc-docs/cmip6/models/csv/$specialization*.csv
-		rm $ESDOC_DIR_REPOS/esdoc-docs/cmip6/models/config/$specialization.json
-		rm $ESDOC_DIR_REPOS/esdoc-docs/cmip6/models/mindmaps/$specialization.mm
-		rm $ESDOC_DIR_REPOS/esdoc-docs/cmip6/models/cmip5-mappings/$specialization.csv
+		cp $ESDOC_DIR_CMIP6/cmip6-specializations-$specialization/$specialization*.py $ESDOC_DIR_REPOS/esdoc-py-client/pyesdoc/mp/specializations/cmip6
 	done
 
+	log "CMIP6-SPECS : syncing csv files ..."
 	for specialization in "${CMIP6_SPECIALIZATIONS[@]}"
 	do
-		log "CMIP6-SPECS : syncing py files ..."
-		cp $ESDOC_DIR_CMIP6/cmip6-specializations-$specialization/$specialization*.py $ESDOC_DIR_REPOS/esdoc-py-client/pyesdoc/mp/specializations/cmip6
-
-		# log "CMIP6-SPECS : syncing csv files ..."
+		rm $ESDOC_DIR_REPOS/esdoc-docs/cmip6/models/csv/$specialization-ids-level-1.csv
+		rm $ESDOC_DIR_REPOS/esdoc-docs/cmip6/models/csv/$specialization-ids-level-2.csv
+		rm $ESDOC_DIR_REPOS/esdoc-docs/cmip6/models/csv/$specialization-ids-level-3.csv
 		cp $ESDOC_DIR_CMIP6/cmip6-specializations-$specialization/_$specialization-ids-level-1.csv $ESDOC_DIR_REPOS/esdoc-docs/cmip6/models/csv/$specialization-ids-level-1.csv
 		cp $ESDOC_DIR_CMIP6/cmip6-specializations-$specialization/_$specialization-ids-level-2.csv $ESDOC_DIR_REPOS/esdoc-docs/cmip6/models/csv/$specialization-ids-level-2.csv
 		cp $ESDOC_DIR_CMIP6/cmip6-specializations-$specialization/_$specialization-ids-level-3.csv $ESDOC_DIR_REPOS/esdoc-docs/cmip6/models/csv/$specialization-ids-level-3.csv
+	done
 
-		# log "CMIP6-SPECS : syncing json files ..."
+	log "CMIP6-SPECS : syncing json files ..."
+	for specialization in "${CMIP6_SPECIALIZATIONS[@]}"
+	do
+		rm $ESDOC_DIR_REPOS/esdoc-docs/cmip6/models/config/$specialization.json
 		cp $ESDOC_DIR_CMIP6/cmip6-specializations-$specialization/_$specialization.json $ESDOC_DIR_REPOS/esdoc-docs/cmip6/models/config/$specialization.json
+	done
 
-		# log "CMIP6-SPECS : syncing mapping files ..."
-		cp $ESDOC_DIR_CMIP6/cmip6-specializations-$specialization/mappings/$specialization-cmip5-to-cmip6-mappings.csv $ESDOC_DIR_REPOS/esdoc-docs/cmip6/models/cmip5-mappings/$specialization.csv
-
-		# log "CMIP6-SPECS : syncing mindmap files ..."
+	log "CMIP6-SPECS : syncing mindmap files ..."
+	for specialization in "${CMIP6_SPECIALIZATIONS[@]}"
+	do
+		rm $ESDOC_DIR_REPOS/esdoc-docs/cmip6/models/mindmaps/$specialization.mm
 		cp $ESDOC_DIR_CMIP6/cmip6-specializations-$specialization/_$specialization.mm $ESDOC_DIR_REPOS/esdoc-docs/cmip6/models/mindmaps/$specialization.mm
+	done
 
+	log "CMIP6-SPECS : syncing cmip5 mapping files ..."
+	for specialization in "${CMIP6_SPECIALIZATIONS[@]}"
+	do
+		rm $ESDOC_DIR_REPOS/esdoc-docs/cmip6/models/cmip5-mappings/$specialization-cmip5-to-cmip6-mappings.csv
+		cp $ESDOC_DIR_CMIP6/cmip6-specializations-$specialization/mappings/$specialization-cmip5-to-cmip6-mappings.csv $ESDOC_DIR_REPOS/esdoc-docs/cmip6/models/cmip5-mappings/$specialization-cmip5-to-cmip6-mappings.csv
 	done
 }
 
