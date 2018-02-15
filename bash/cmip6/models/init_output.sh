28ed3a6b6bdbefdd45326309c0e4759f90057b50
Mark Morgan <momipsl@ipsl.jussieu.fr>
2018-02-15 17:33:21 +0100

    Latest cmip5 to cmip6 mappings

--- bash/cmip6/models/init_output.sh
+++ bash/cmip6/models/init_output.sh
@@ -12,6 +12,7 @@ main()
 	if [ $1 = "all" ]; then
 		for institution_id in "${INSTITUTION_ID[@]}"
 		do
+			log "... initializing "$institution_id
 			python $ESDOC_HOME/bash/cmip6/models/init_output --institution-id=$institution_id
 		done
 	else
